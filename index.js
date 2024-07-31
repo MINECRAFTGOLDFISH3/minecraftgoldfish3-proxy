9baf17c61ba577f5ca4f3728a73ab0ff57a035c9
MINECRAFTGOLDFISH3 <152801036+MINECRAFTGOLDFISH3@users.noreply.github.com>
2024-07-31 15:56:30 -0500

    Add 404

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ chemical.app.use(express.static("public", {
     extensions: ["html"]
 }));
 
+chemical.error((req, res) => {
+    res.status(404);
+    res.send("404 Error");
+});
+
 chemical.listen(port, () => {
     console.log(`Chemical example styled listening on port ${port}`);
 });
